0b7edfd72c4a72655b4ccabf98a403cb6b3724f6
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-07-02 12:23:14 +0530

    Add files via upload

--- OLYMPIC LOGO/main.js
+++ OLYMPIC LOGO/main.js
@@ -1,43 +1,38 @@
-canvas -
-document.getElementById("myCanvas")
+canvas =document.getElementById("myCanvas")
 ctx=canvas.getContext("2d")
-
-function draw(){
-ctx = document.getElementById;
-}
 
 ctx.beginPath();
 ctx.stokeStyle = "grey";
-ctx.lineWidth = 5;
+ctx.lineWidth = 7;
 ctx.rect(150, 143, 430, 200)
-ctx.stoke();
+ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "Blue";
-ctx.lineWidth = 5;
-ctx.arc(200, 200, 40, 0, 2*Math.PI)
-ctx.stoke();
+ctx.lineWidth = 7;
+ctx.arc(250, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "Black";
-ctx.lineWidth = 5;
-ctx.arc(400, 200, 40, 0, 2*Math.PI)
-ctx.stoke();
+ctx.lineWidth = 7;
+ctx.arc(350, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "Red";
-ctx.lineWidth = 5;
-ctx.arc(600, 200, 40, 0, 2*Math.PI)
-ctx.stoke();
+ctx.lineWidth = 7;
+ctx.arc(450, 210, 40, 0, 2*Math.PI)
+ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "Yellow";
-ctx.lineWidth = 5;
-ctx.arc(300, 500, 40, 0, 2*Math.PI)
-ctx.stoke();
+ctx.lineWidth = 7;
+ctx.arc(300, 250, 40, 0, 2*Math.PI)
+ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle = "Green";
-ctx.lineWidth = 5;
-ctx.arc(500, 500, 40, 0, 2*Math.PI)
-ctx.stoke();
+ctx.lineWidth = 7;
+ctx.arc(400, 250, 40, 0, 2*Math.PI)
+ctx.stroke();
